5f48aa495f2ecd91be31858be8bc62c6c2701a0c
Lusa1101 <omphu.shau@outlook.com>
2025-04-24 07:11:26 +0200

    Adding Task1 application

---
+++ Apps/Task1/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.Licensing;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace Task1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            //Licensing
+            SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBPh8sVXJ9S0d+X1JPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9mSXpSckVhXXhcdHBSRmhXU00=;NRAiBiAaIQQuGjN/V09+XU9HdVRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS3tTckRnWX1ad3ZRQ2lZWU90Vg==;Mgo+DSMBMAY9C3t2XFhhQlJHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH5Xd0FjW3tZcnBSRGFcWkZ/;MzgxNDIwMEAzMjM5MmUzMDJlMzAzYjMyMzkzYmJPcTJuRjR1b3pUaUc1TStlVlpCR3BmVEhON1hDMGhzN2V2cFVtZjAxbTA9;MzgxNDIwMUAzMjM5MmUzMDJlMzAzYjMyMzkzYkxHMHR0bnBDeFJRbUpkMXRPTUNGM2xHZHBLN1VFV1E4RytuQ09mSEN5aUk9");
+        }
+    }
+}
---
+++ Apps/Task1/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text;
+
+namespace Task1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmitClient(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    public partial class ClientViewModel : ObservableObject
+    {
+        //DB
+        Database db = new();
+
+        //Command for insert button
+        public ICommand InsertCommand { get; set; }
+
+        //The client list
+        [ObservableProperty]
+        ObservableCollection<Client> clients = new();
+
+        //For the inputs
+        [ObservableProperty]
+        string name = string.Empty;
+        [ObservableProperty]
+        string cell = string.Empty;
+        [ObservableProperty]
+        string address = string.Empty;
+
+        public ClientViewModel()
+        {
+            //List
+            Update();
+
+            //For the commamd
+            InsertCommand = new Command(async () => await Insert());
+        }
+
+        //Function for inserting
+        async Task Insert()
+        {
+            Client client = new Client();
+
+            if (Name != string.Empty && Name.Length > 0 &&
+                Address != string.Empty && Address.Length > 0 &&
+                Cell != string.Empty && Cell.Length > 0)
+            {
+                //Copy the input to the client variable
+                client.Name =   Name;
+                client.Address = Address;
+                client.Phone_Number = Cell;
+                client.Client_id = Clients.Max(c => c.Client_id) + 1;
+
+                //Add to the list
+                var output = await db.InsertClient(client);
+
+                //Update the list
+                await Update();
+
+                //Nullify the entries
+                Name = string.Empty;
+                Address = string.Empty;
+                Cell = string.Empty;
+            }
+        }
+
+        //Data
+        async Task Update()
+        {
+            var temp = new List<Client>
+            {
+                new Client { Client_id = 83774873, Name = "Omphulusa", Phone_Number = "0782938928", Address = "Address 1" },
+                new Client { Client_id = 82773883, Name= "Ndivhuwo", Phone_Number = "0783672893", Address = "Address 2" }
+            };
+
+            var list = await db.GetClients();
+
+            Clients = new ObservableCollection<Client>(list);
+        }
+    }
+
+    public class Client
+    {
+        public Client ()
+        {
+
+        }
+
+        public int Client_id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Phone_Number { get; set; } = string.Empty;
+
+    }
+
+    public class Database
+    {
+        HttpClient client = new HttpClient();
+        public Database()
+        {
+            //use ip address if external devices are used
+            if (DeviceInfo.Platform == DevicePlatform.WinUI)
+                client.BaseAddress = new Uri("https://localhost:7069/api/");
+            else
+                client.BaseAddress = new Uri("https://192.168.56.1:7069/api/");
+        }
+
+        public async Task<List<Client>> GetClients()
+        {
+            var response = await client.GetAsync("Client/GetAll");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var list = JsonConvert.DeserializeObject<List<Client>>(data); // Deserialize JSON to your model
+
+                if (list is not null)
+                return list.ToList();// Use the products list in your app
+            }
+            else
+            {
+                Debug.WriteLine($"Error: {response.StatusCode}");
+            }
+
+            return new();
+        }
+
+        public async Task<string> InsertClient(Client client)
+        {
+            var json_content = new StringContent(JsonConvert.SerializeObject(client), Encoding.UTF8, "application/json");
+
+            try
+            {
+                var responce = await this.client.PostAsync("Client/AddNewClient", json_content);
+                var responceBody = await responce.Content.ReadAsStringAsync();
+
+                return responceBody;
+            }
+            catch (Exception e)
+            {
+                return ("Inserting new client:\n" + e.Message);
+            }
+        }
+    }
+
+}
